12b1ddf27092913fcee08716da149933d0cf8e83
Lamp <90402655+Lamp1337@users.noreply.github.com>
2021-10-04 00:25:44 -0700

    Better Code and Added def function

--- main.py
+++ main.py
@@ -1,76 +1,71 @@
-#Converter Python & Javascript to Exe
-#Coded by Lamp, (C) NumeXx
+#Coded by Lamp (C) Numex
+import os, sys
 
-#import module
-import os
-import time
+s = """
+[+]===================================[+]
+[+] Coded by Lamp#1442                [+]
+[+] Credit to Numex                   [+]
+[+]===================================[+]
+[+] 1. Install Package Python to Exe
+[+] 2. Install Package JS to Exe
+[+] 3. Convert Python to Exe
+[+] 4. Convert Javascript to Exe
+[+] 5. Exit
+[+]===================================[+] """
 
-#Let's goooo
-tolol = """
-[#]===================================[#]
-[+] Converter Coded by Lamp
-[+] Credit : NumeXx
-[#]===================================[#]
-[+] 1. Install Package JS to Exe
-[+] 2. Install Package PY to Exe
-[+] 3. Convert JS to Exe
-[+] 4. Convert PY to Exe
-[+] 5. Close Program
-[!] Must Install Node.js or Python!
-[#]===================================[#]
-"""
-
-print(tolol)
-choice = input("[#] Enter Your Choice : ")
-if choice == "1":
-    print("Installing Package...")
-    time.sleep(1)
-    print("Please Wait..")
-    os.system('npm install -g pkg')
-    print("Successfuly Install Package")
-    input("=*= Press 'Enter' to Continue: ")
+def pkgpy():
+    print("Installing.. Please wait")
+    os.system("pip install pyinstaller")
+    print("Successfuly install Python Package!")
+    input("Press 'Enter' to Back :")
     os.system('cls')
-    os.system('python Converter.py')
+    main()
 
-elif choice == "2":
-    print("Installing Package...")
-    time.sleep(1)
-    print("Please Wait..")
-    os.system('pip install pyinstaller')
-    print("Successfuly Install Package")
-    input("=*= Press 'Enter' to Continue: ")
+def pkgjs():
+    print("Installing.. Please wait")
+    os.system("npm i -g pkg")
+    print("Successfuly install Javascript Package!")
+    input("Press 'Enter' to Back :")
     os.system('cls')
-    os.system('python Converter.py')
+    main()
 
-elif choice == "3":
-    print("[!] Example : main.js")
-    conv = input("[#] Enter Your JS File Name : ")
-    print("Converting File..")
-    time.sleep(1)
-    os.system(f"pkg {conv}")
-    print("Successfuly Converting JS to Exe!")
-    input("=*= Press 'Enter' to Continue: ")
-    os.system('cls')
-    os.system('python Converter.py')
+def convpy():
+    print("Example = main.py")
+    cpy = input("Enter Python file name : ")
+    if cpy == "":
+        print("Please enter the file name!")
+        convpy()
+    else :
+        os.system(f"pyinstaller {cpy} --onefile")
+        print("Successfuly Convert python to exe")
+        input("Press 'Enter' to Back :")
+        os.system('cls')
+        main()
 
-elif choice == "4":
-    print("[!] Example : main.py")
-    conv2 = input("[#] Enter Your PY File Name : ")
-    print("Converting File..")
-    time.sleep(1)
-    os.system(f"pyinstaller {conv2} --onefile")
-    print("Successfuly Converting PY to Exe!")
-    input("=*= Press 'Enter' to Continue: ")
-    os.system('cls')
-    os.system('python Converter.py')
+def convjs():
+    print("Example = index.js")
+    cjs = input("Enter Js file name : ")
+    if cjs == "":
+        print("Please enter the file name!")
+    else:
+        os.system(f"pkg {cjs}")
+        print("Successfuly Convert javascript to exe")
+        input("Press 'Enter' to Back :")
+        os.system('cls')
+        main()
 
+def main():
+    print(s,"\n")
+    m = input("[?] Enter Your Choice : ")
+    if m == "1":
+        pkgpy()
+    elif m == "2":
+        pkgjs()
+    elif m == "3":
+        convjs()
+    elif m == "4":
+        convpy()
+    elif m == "5":
+        sys.exit()
 
-elif choice == "5":
-    exit()
-
-else :
-    os.system('cls')
-    print("You Misstyped! Please Try Again.")
-    time.sleep(1)
-    os.system('python Converter.py')
-    exit()
+main()
